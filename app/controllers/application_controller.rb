a709463d45495956b1ba994ce28c6c58c6df29b7
Rutuja Deshkar <rutuja.deshkar@gmail.com>
2020-01-15 07:50:32 +0100

    JWT changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::API
   # respond_to :json
 
   def render_resource(resource)
+    p "resource #{resource.inspect}"
     if resource.errors.empty?
       render json: resource
     else
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,17 +4,23 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :jwt_authenticatable, jwt_revocation_strategy: JwtBlacklist
+  # before_save :ensure_authentication_token
 
 
-  # def after_database_authentication
-  #   @token = Devise.friendly_token
-  #   current_user.user_token = @token
-  #   current_user.token_valid_time_in_minutes = 60
-  #   current_user.token_validity_end_by = Time.now + 60  * 60
-  #   current_user.save
-  #   format.json { render json: @token }
-  # end
+  def after_database_authentication
+    p "creating token"
+    @token = Devise.friendly_token
+    p "token #{@token}"
+    current_user.authentication_token = @token
+    # current_user.token_valid_time_in_minutes = 60
+    # current_user.token_validity_end_by = Time.now + 60  * 60
+    current_user.save
+    p "current_user #{current_user}"
+    format.json { render json: @token }
+  end
 
   has_many_attached :files
   validates :email, :encrypted_password, presence: true
+
+
 end
